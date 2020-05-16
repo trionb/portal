8f23b9d2ffd8d4629744aba59519c1402baac663
trion bailey <trionbailey@gmail.com>
2020-05-16 11:29:53 -0400

    Updates

---
+++ precache-manifest.2c8b8d53ee9a9a76c11ecad812be1693.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b171fce8e4e485d04fab0528105b090",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "162c1531103d99df79af",
+    "url": "/portfolio/static/css/main.14f2cbdd.chunk.css"
+  },
+  {
+    "revision": "3f687c0d6f5ad89b1e92",
+    "url": "/portfolio/static/js/2.68f5bd4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.68f5bd4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162c1531103d99df79af",
+    "url": "/portfolio/static/js/main.f2467a41.chunk.js"
+  },
+  {
+    "revision": "05a9d53445ec485b3566",
+    "url": "/portfolio/static/js/runtime-main.5b2ca5c3.js"
+  },
+  {
+    "revision": "389d63c732726a58de63892bc7630967",
+    "url": "/portfolio/static/media/Employee Generator.389d63c7.png"
+  },
+  {
+    "revision": "6d0a3eb99618b6ca58dc98e63df2e829",
+    "url": "/portfolio/static/media/fyre.6d0a3eb9.png"
+  },
+  {
+    "revision": "82a27040077e1ab8fec03bf850579f89",
+    "url": "/portfolio/static/media/me.82a27040.jpeg"
+  },
+  {
+    "revision": "4461c86d4b8d1cd3779156c7d5985841",
+    "url": "/portfolio/static/media/modern.4461c86d.png"
+  },
+  {
+    "revision": "1d6d7d3e36887af743a76b323b524e88",
+    "url": "/portfolio/static/media/note-taker.1d6d7d3e.png"
+  },
+  {
+    "revision": "615300605ab41e482eb114a491c9e6ac",
+    "url": "/portfolio/static/media/passwordgenerator.61530060.png"
+  },
+  {
+    "revision": "4e7a8112a1b1d80d8c9c7e1d01c4c9fa",
+    "url": "/portfolio/static/media/solveStaking.4e7a8112.png"
+  },
+  {
+    "revision": "72403ae07a457b9e0dc364c6fac34a08",
+    "url": "/portfolio/static/media/soundtrackmovie.72403ae0.png"
+  },
+  {
+    "revision": "a130bb8612f82414f46bd1353fc7649d",
+    "url": "/portfolio/static/media/sushi.a130bb86.png"
+  }
+]);
